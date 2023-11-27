ea01ffae5da694a02f21d6d18991dc24b189f087
Marcel Roesink <mroesink@rocvantwente.nl>
2023-11-27 15:51:18 +0100

    Initial commit

---
+++ I0SD_Toets1_Start/MyFunctions.cs
@@ -0,0 +1,10 @@
+﻿namespace I0SD_Toets1_Start
+{
+    public class MyFunctions
+    {
+        public  static string Hallo()
+        {
+            return "Welkom bij deze ASP-NET / C# toets";
+        }
+    }
+}
